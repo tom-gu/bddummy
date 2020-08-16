38a13ebf8bb6760444b7edb0fcb0e1ab261ab175
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-08-16 20:23:06 +0530

    Update test-foo.R

--- tests/testthat/test-foo.R
+++ tests/testthat/test-foo.R
@@ -3,22 +3,7 @@ test_that("multiplication works", {
   expect_equal(2 * 2, 4)
 })
 
-test_that("bdtest master", {
-  expect_equal(bdtests::foo(1), 2)
-  # foo_bddummy: x = x+1
-})
-
 test_that("bdtest dev", {
   expect_equal(bdtests::foo(1), 101)
-  # foo_bddummy: x = x+1
-})
-
-test_that("bddummy master", {
-  expect_equal(foo_bddummy(1), 2)
-  # foo_bddummy: x = x+1
-})
-
-test_that("bddummy dev", {
-  expect_equal(foo_bddummy(1), 101)
   # foo_bddummy: x = x+1
 })
