f90a1797939c5c501cce9dd6ca12e67cf01b2338
Sunny Dhoke <sunnydhoke22@gmail.com>
2020-08-16 19:23:40 +0530

    Update test-foo.R

--- tests/testthat/test-foo.R
+++ tests/testthat/test-foo.R
@@ -12,3 +12,13 @@ test_that("bdtest dev", {
   expect_equal(bdtests::foo(1), 101)
   # foo_bddummy: x = x+1
 })
+
+test_that("bddummy master", {
+  expect_equal(foo_bddummy(1), 2)
+  # foo_bddummy: x = x+1
+})
+
+test_that("bddummy dev", {
+  expect_equal(foo_bddummy(1), 101)
+  # foo_bddummy: x = x+1
+})
